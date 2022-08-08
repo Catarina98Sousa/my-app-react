58f052dac6385dd5e1a1e4b40b1c1f6761e03e6b
Catarina Sousa <76219394+Catarina98Sousa@users.noreply.github.com>
2022-08-08 22:55:53 +0100

    my bad

--- src/components/BasicCard/index.js
+++ src/components/BasicCard/index.js
@@ -17,7 +17,7 @@ function BasicCard(props) {
             (<img
             src={ImgAlt} alt="" className="basic-card__img"/>)}
             <div className="basic-card__body">
-                {props.date && <span>{myDate}</span>}
+                {props.date && <span>{props.date}</span>}
                 <h5>{props.title}</h5>
                 <p className="basic-card__description">{props.description}</p>
                 <a href={props.url}>{props.link}</a>
