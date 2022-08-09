1b873363098958d35ad61ce18ed8a96fd06e3827
Catarina Sousa <76219394+Catarina98Sousa@users.noreply.github.com>
2022-08-09 01:08:43 +0100

    date

--- src/components/BasicCard/index.js
+++ src/components/BasicCard/index.js
@@ -1,12 +1,14 @@
 import "./basic-card.css";
 import ImgAlt from "../../image/kawaiicat.png";
 
-/* const myDate = new Date(article.created_at).toLocaleDateString("en-US",{
+
+function BasicCard(props) {
+    
+    const myDate = new Date(props.date).toLocaleDateString("en-US",{
     day:'numeric',
     month:'long',
     year: 'numeric'
-}); */
-function BasicCard(props) {
+});
     return (
         <div className="basic-card" id={props.id}>
             {props.imgPath? (<img
@@ -17,7 +19,7 @@ function BasicCard(props) {
             (<img
             src={ImgAlt} alt="" className="basic-card__img"/>)}
             <div className="basic-card__body">
-                {props.date && <span>{props.date}</span>}
+                {props.date && <span>{myDate}</span>}
                 <h5>{props.title}</h5>
                 <p className="basic-card__description">{props.description}</p>
                 <a href={props.url}>{props.link}</a>
